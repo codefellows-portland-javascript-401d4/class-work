186f95cca783a7e4cc2c3b09cad3f32fbe50b0ae
martypdx <marty@martypdx.com>
2016-12-13 11:54:04 -0800

    add in ui-router examples

--- week8_scss_routing_services/dec13_review-and-ui-router/app/src/routes.js
+++ week8_scss_routing_services/dec13_review-and-ui-router/app/src/routes.js
@@ -9,9 +9,54 @@ export default function routes($stateProvider, $urlRouterProvider) {
     });
 
     $stateProvider.state({
+        name: 'welcome.new',
+        url: 'new',
+        template: '<p>Hello New Person</p>'
+    });
+
+    $stateProvider.state({
+        name: 'welcome.returning',
+        url: 'returning',
+        template: '<p>Welcome Back</p>'
+    });
+
+    $stateProvider.state({
         name: 'pirates', // name of this defined app "state"
         url: '/pirates', // url associated with the state,
         component: 'pirates' 
+    });
+
+    $stateProvider.state({
+        name: 'about',
+        url: '/about',
+        component: 'about'
+    });
+
+    $stateProvider.state({
+        name: 'about.plain',
+        url: '/plain',
+        views: {
+            header: {
+                component: 'aboutHeader'
+            },
+            main: {
+                component: 'aboutMain'
+            }
+        }
+    });
+
+
+    $stateProvider.state({
+        name: 'about.special',
+        url: '/special',
+        views: {
+            header: {
+                component: 'aboutHeaderSpecial'
+            },
+            main: {
+                component: 'aboutMainSpecial'
+            }
+        }
     });
 
     $urlRouterProvider.otherwise('/');
